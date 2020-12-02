70ed2330238f89002399bdedeff31a0e523cbdcb
DESKTOP-GQEQP04\Natan Nobre <natannobre37@gmail.com>
2020-12-02 19:54:05 -0300

    UDP Communication

---
+++ UDPCommunication/UDPClient.py
@@ -0,0 +1,14 @@
+import socket
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+strMESSAGE = "Hello World!"
+byteMESSAGE = str.encode(strMESSAGE)
+
+print("UDP target IP: ", UDP_IP)
+print("UDP target port: ", UDP_PORT)
+print("message: ", strMESSAGE)
+
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+sock.sendto(byteMESSAGE, (UDP_IP, UDP_PORT))
