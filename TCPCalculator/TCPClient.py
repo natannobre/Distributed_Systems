d5f335954afe109101e9a989e2d637c8b64ded99
DESKTOP-GQEQP04\Natan Nobre <natannobre37@gmail.com>
2020-12-16 02:08:48 -0300

    Calculator implemented using TCP

--- TCPCalculator/TCPClient.py
+++ TCPCalculator/TCPClient.py
@@ -1,11 +1,18 @@
 #AUTHOR: NATAN NOBRE CHAVES - COMPUTER ENGINEERING
 
+# MODEL TO REQUEST: 10 + 2
+# NUM[SPACE]SYMBOL[SPACE]NUM
+#
+# NUMBERS ALLOWED: ANY RATIONAL NUMBER
+# SYMBOLS ALLOWED: +,-,* AND /
+
 import socket
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-strMESSAGE = "Hello World!"
+
+strMESSAGE = input()
 byteMESSAGE = str.encode(strMESSAGE)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #STREAM = TCP / DATAGRAM = UDP
@@ -14,4 +21,6 @@ s.send(byteMESSAGE)
 data = s.recv(BUFFER_SIZE)
 s.close()
 
-print("received data:", data.decode())
+data = float(data.decode())
+data = '{0:g}'.format(data)
+print("Result:", data)
---
+++ TCPCalculator/TCPServer.py
@@ -0,0 +1,38 @@
+#AUTHOR: NATAN NOBRE CHAVES - COMPUTER ENGINEERING
+
+import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 1024
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print ("Connection address:", addr)
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+
+    list_Data = data.split(str.encode(' '))
+    op = list_Data[1].decode()
+    result = float(0)
+
+    # Calculator
+    if op is '+':
+        result = float(list_Data[0]) + float(list_Data[2])
+
+    elif op == '-':
+        result = float(list_Data[0]) - float(list_Data[2])
+
+    elif op == '*':
+        result = float(list_Data[0]) * float(list_Data[2])
+
+    elif op == '/':
+        result = float(list_Data[0]) / float(list_Data[2])
+
+    result = str.encode(str(result))
+    conn.send(result)
+conn.close()
